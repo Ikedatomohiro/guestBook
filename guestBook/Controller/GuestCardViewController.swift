68e5b3bbb741ab5014c1c3da09535002ca9c5e16
tomohiro <to.tomo.tomohiro@gmail.com>
2020-11-05 22:34:37 +0900

    芳名カードのラベル作成

--- guestBook/Controller/GuestCardViewController.swift
+++ guestBook/Controller/GuestCardViewController.swift
@@ -11,7 +11,6 @@ import PencilKit
 class GuestCardViewController: UIViewController {
     
     fileprivate let event: Event
-
     init(event: Event) {
         self.event = event
         super.init(nibName: nil, bundle: nil)
@@ -19,12 +18,71 @@ class GuestCardViewController: UIViewController {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+
+    fileprivate let cardTitleLabel = UILabel()
+    fileprivate let selectRitualLabel = UILabel()
+    fileprivate let retualArray:[String] = ["通夜", "告別式"]
+    fileprivate let guestNameLabel = UILabel()
+    fileprivate let companyNameLabel = UILabel()
+    fileprivate let zipCodeLabel = UILabel()
+    fileprivate let telLabel = UILabel()
+    fileprivate let addressLabel = UILabel()
+    fileprivate let selectAcuaintanceQuestionLabel = UILabel()
+    fileprivate let selectAcuaintanceLabel = UILabel()
+    fileprivate let selectRelationQuestionLabel = UILabel()
+    fileprivate let selectRelationLabel = UILabel()
+
+    
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.view.backgroundColor = .purple
+        setCanvas()
         
+
+    }
+    
+    fileprivate func setupLabels() {
+        view.addSubview(cardTitleLabel)
+        cardTitleLabel.text = "ご芳名カード"
+        
+        
+        view.addSubview(selectRitualLabel)
+        
+        
+        
+        view.addSubview(guestNameLabel)
+        
+        
+        
+        view.addSubview(companyNameLabel)
+        
+        
+        
+        view.addSubview(zipCodeLabel)
+        
+        
+        
+        view.addSubview(telLabel)
+        
+        
+        
+        view.addSubview(addressLabel)
+        view.addSubview(selectAcuaintanceQuestionLabel)
+        view.addSubview(selectAcuaintanceLabel)
+        view.addSubview(selectRelationQuestionLabel)
+        view.addSubview(selectRelationLabel)
+        
+        
+        
+        
+    }
+    
+    fileprivate func setCanvas() {
         let canvas = PKCanvasView(frame: view.frame)
         view.addSubview(canvas)
         canvas.tool = PKInkingTool(.pen, color: .black, width: 30)
@@ -38,5 +96,4 @@ class GuestCardViewController: UIViewController {
             }
         }
     }
-
 }
