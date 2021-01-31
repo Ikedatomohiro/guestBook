16a8c3986994182a7297c57384222e9b9ab5d921
Ikedatomohiro <to.tomo.tomohiro@gmail.com>
2021-01-31 23:19:53 +0900

    ゲスト一覧の文字表示のテスト

--- guestBook/View/GuestCell.swift
+++ guestBook/View/GuestCell.swift
@@ -9,16 +9,29 @@ import UIKit
 
 class GuestCell: UITableViewCell {
     
+    fileprivate var guestTextLabel = UILabel()
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setup()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+
     }
     
     func setupGuestData(guest: Guest) {
         self.textLabel?.text = guest.guestName
     }
+
+
+    fileprivate func setup() {
+        contentView.addSubview(guestTextLabel)
+        guestTextLabel.anchor(top: layoutMarginsGuide.topAnchor, leading: nil, bottom: nil, trailing: nil)
+        guestTextLabel.text = "練習のラベル"
+    }
+
+
 }
 
--- guestBook/View/GuestListHeaderCell.swift
+++ guestBook/View/GuestListHeaderCell.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class GuestListHeaderCell: UITableViewCell {
     
+    fileprivate let numberLabel = UILabel()
+    fileprivate let guestNameLabel = UILabel()
+    fileprivate let companyNamelabel = UILabel()
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setup()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+
+    fileprivate func setup() {
+        contentView.addSubview(numberLabel)
+        numberLabel.anchor(top: layoutMarginsGuide.topAnchor, leading: layoutMarginsGuide.leadingAnchor, bottom: layoutMarginsGuide.bottomAnchor, trailing: nil)
+        numberLabel.text = "No."
+        
+        
+        contentView.addSubview(guestNameLabel)
+        guestNameLabel.anchor(top: layoutMarginsGuide.topAnchor, leading: numberLabel.trailingAnchor, bottom: layoutMarginsGuide.bottomAnchor, trailing: nil)
+        guestNameLabel.text = "御芳名"
+        
+        
+        contentView.addSubview(companyNamelabel)
+        companyNamelabel.anchor(top: layoutMarginsGuide.topAnchor, leading: guestNameLabel.trailingAnchor, bottom: layoutMarginsGuide.bottomAnchor, trailing: nil)
+        companyNamelabel.text = "会社名"
+        
+        
+    }
+
 }
