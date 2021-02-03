217461beded3bd7fd15e32d5af7d3ab46eb67857
Ikedatomohiro <to.tomo.tomohiro@gmail.com>
2021-02-03 21:52:51 +0900

    画像のURLを作って画像解析APIを実行する

--- guestBook/Controller/SettingViewController.swift
+++ guestBook/Controller/SettingViewController.swift
@@ -7,8 +7,7 @@
 
 import UIKit
 import Alamofire
-
-
+import FirebaseStorage
 
 class SettingViewController: UIViewController {
     var points: [CGPoint]!
@@ -17,122 +16,50 @@ class SettingViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .blue
-        
-//        let task = URLSession.shared.dataTask(with: makeRequest(), completionHandler: { (data, response, err) in
-//          UIApplication.shared.isNetworkActivityIndicatorVisible = false
-//          guard err == nil else {
-//            DispatchQueue.main.async {
-//              self.showErrorAlert()
-//            }
-//            return
-//          }
-//          do {
-//              let json = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.allowFragments) as! [Any]
-//              let jsonMap = json.map { (jsonMap) -> [String: Any] in
-//                  return jsonMap as! [String: Any]
-//              }
-//              print(jsonMap)
-//          }
-//          catch {
-//              print(error)
-//          }
-//        })
-//        UIApplication.shared.isNetworkActivityIndicatorVisible = true
-//        task.resume()
-    
-    
-    
-    
-//    }
-    
-//    func makeRequest() -> URLRequest {
-//      let json = ["requests": [
-//      [
-//        "image": [
-//          "source": [
-////            "imageUri": "gs://guest_book/UPLOADFOLDER.png"
-//            "imageUri": "gs://vision-api-handwriting-ocr-bucket/handwriting_image.png"
-//          ]
-//        ],
-//        "features": [
-//          [
-//            "type": "DOCUMENT_TEXT_DETECTION"
-//          ]
-//        ]
-//      ]
-//    ]
-//                 ]
-//      let request = NSMutableURLRequest(url: URL(string: "https://vision.googleapis.com/v1/images:annotate?key=\(googleAPIKey)")!)
-//      request.httpMethod = "POST"
-//      if JSONSerialization.isValidJSONObject(json) {
-//          request.httpBody = try! JSONSerialization.data(withJSONObject: json, options: .prettyPrinted)
-//      }
-//      request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-//      request.addValue("application/json", forHTTPHeaderField: "Accept")
-//      return request as URLRequest
-//    }
-//    func showErrorAlert() {
-//      let alertController = UIAlertController(title: "Error", message: "Something went wrong 😔", preferredStyle: .alert);
-//      let okAction = UIAlertAction(title: "OK", style: .default, handler: nil);
-//      alertController.addAction(okAction);
-//      self.present(alertController, animated: true, completion: nil);
-        
-        
         callGoogleVisionApi()
     }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-
-    }
-    
-    
-    
     
     fileprivate func callGoogleVisionApi() {
         
         let apiURL = "https://vision.googleapis.com/v1/images:annotate?key=\(googleAPIKey)"
-        let imageUri = "https://storage.googleapis.com/guest_info/技術者のためのやさしい数学.png"
-//        let imageUri = "gs://guestbook-311bf.appspot.com/QFQSYdXfzaUceWlWfWsj_guestName.png"
-//        let imageUri = "https://storage.googleapis.com/guest_info/QFQSYdXfzaUceWlWfWsj_guestName-6.png"
-
-        let parameters: Parameters = [
-            "requests": [
-                "image": [
-                    "source": ["imageUri": imageUri
+        var imageUri = ""
+        FirebaseStorage.Storage.storage().reference().child("QFQSYdXfzaUceWlWfWsj_guestName.png").downloadURL { (URL, error) in
+            // 画像URLを取得できたときに画像解析を行う
+            if URL != nil {
+                imageUri = URL?.absoluteString ?? ""
+                let parameters: Parameters = [
+                    "requests": [
+                        "image": [
+                            "source": ["imageUri": imageUri
+                            ]
+                        ],
+                        "features": [
+                            "type": "TEXT_DETECTION",
+                            "maxResults": 1
+                        ],
+                        "imageContext": [
+                            "languageHints": [
+                            "ja-t-i0-handwrit"
+                          ]
+                        ]
                     ]
-                ],
-                "features": [
-                    "type": "TEXT_DETECTION",
-                    "maxResults": 1
-                ],
-                "imageContext": [
-                    "languageHints": [
-                    "ja-t-i0-handwrit"
-                  ]
                 ]
-            ]
-        ]
 
-        let headers: HTTPHeaders = [
-            "Content-Type": "application/json",
-            "X-Ios-Bundle-Identifier": Bundle.main.bundleIdentifier ?? ""]
-
-//        AF.request(apiURL, method: .post, parameters: parameters)
-        
-        
-        AF.request(
-            apiURL,
-            method: .post,
-            parameters: parameters,
-            encoding: JSONEncoding.default,
-            headers: headers)
-            .responseJSON { response in
-//                if response.result {
-//                    completion(nil)
-                debugPrint(response)
-                    return
-//                }
-                print(response.result)
+                let headers: HTTPHeaders = [
+                    "Content-Type": "application/json",
+                    "X-Ios-Bundle-Identifier": Bundle.main.bundleIdentifier ?? ""]
+                AF.request(
+                    apiURL,
+                    method: .post,
+                    parameters: parameters,
+                    encoding: JSONEncoding.default,
+                    headers: headers)
+                    .responseJSON { response in
+                        debugPrint(response)
+                        return
+                }
+            }
+            print(URL ?? "画像が見つかりません")
         }
     }
     
