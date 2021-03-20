6105c6c050a032065a5fce9a6f829aff0d726e2a
Ikedatomohiro <to.tomo.tomohiro@gmail.com>
2021-03-20 10:21:26 +0900

    詳細ページ作り始め

---
+++ guestBook/View/UITableView/TableView.swift
@@ -0,0 +1,20 @@
+//
+//  TableView.swift
+//  guestBook
+//
+//  Created by Tomohiro Ikeda on 2021/03/20.
+//
+
+import UIKit
+
+class TableView: UITableView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
