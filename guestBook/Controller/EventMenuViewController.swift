9df50f2ce6f3d34d916c072c6f2f6b2e87b19b9a
Ikedatomohiro <to.tomo.tomohiro@gmail.com>
2021-04-25 23:29:34 +0900

    EventMenuControllerの表示内容調整

--- guestBook/Controller/EventMenuViewController.swift
+++ guestBook/Controller/EventMenuViewController.swift
@@ -13,17 +13,19 @@ class EventMenuViewController: UIViewController {
     fileprivate let event: Event
     fileprivate var guests: [Guest]       = []
     fileprivate let moveGuestCardButton   = UIButton()
-    fileprivate let moveGuestListButton = UIButton()
+    fileprivate let moveGuestListButton   = UIButton()
     fileprivate let moveSettingButton     = UIButton()
     fileprivate var retuals: [Retual]     = []
     fileprivate var relations: [Relation] = []
     fileprivate var groups: [Group]       = []
-    fileprivate var pageNumber: Int       = 0
     let selectGuests = SelectGuests()
+    lazy var eventInfoAreaView     = EventInfoAreaView(event: event, guests: guests, frame: .zero)
+    
     init(event: Event) {
         self.event = event
         super.init(nibName: nil, bundle: nil)
     }
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -31,6 +33,7 @@ class EventMenuViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupBase()
+        setupEventInfoArea()
         setupMoveGuestCardButton()
         setupMoveGuestListButton()
         setupMoveSettingButton()
@@ -38,9 +41,7 @@ class EventMenuViewController: UIViewController {
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        selectGuests.fetchData(eventId: event.eventId) { (guests) in
-            self.guests = guests
-        }
+        fetchData()
     }
     
     //MARK:- Function
@@ -50,31 +51,46 @@ class EventMenuViewController: UIViewController {
         self.view.backgroundColor = .white
     }
     
+    fileprivate func setupEventInfoArea() {
+        view.addSubview(eventInfoAreaView)
+        eventInfoAreaView.anchor(top: view.layoutMarginsGuide.topAnchor, leading: view.layoutMarginsGuide.leadingAnchor, bottom: nil, trailing: view.layoutMarginsGuide.trailingAnchor)
+    }
+    
+    fileprivate func fetchData() {
+        selectGuests.fetchData(eventId: event.eventId) { (guests) in
+            self.guests = guests
+            // 参加者の登録数をセット
+            self.eventInfoAreaView.setGuestsCount(count: guests.count)
+        }
+    }
+    
     fileprivate func setupMoveGuestCardButton() {
         view.addSubview(moveGuestCardButton)
-        moveGuestCardButton.setTitle("参加者入力", for: .normal)
-        moveGuestCardButton.backgroundColor = green
-        moveGuestCardButton.anchor(top: view.layoutMarginsGuide.topAnchor, leading: view.layoutMarginsGuide.leadingAnchor, bottom: nil, trailing: nil, padding: .init(top: 50, left: 0, bottom: 0, right: 0), size: .init(width: 150, height: 40))
-        moveGuestCardButton.layer.cornerRadius = 5
+        buttonCustomise(button: moveGuestCardButton, text: "参加者入力")
+        moveGuestCardButton.anchor(top: eventInfoAreaView.bottomAnchor, leading: view.layoutMarginsGuide.leadingAnchor, bottom: nil, trailing: nil, padding: .init(top: 50, left: 0, bottom: 0, right: 0), size: .init(width: 150, height: 50))
         moveGuestCardButton.addTarget(self, action: #selector(showGuestCard), for: .touchUpInside)
     }
     
     fileprivate func setupMoveGuestListButton() {
         view.addSubview(moveGuestListButton)
-        moveGuestListButton.setTitle("参加者一覧", for: .normal)
-        moveGuestListButton.backgroundColor = green
-        moveGuestListButton.anchor(top: moveGuestCardButton.bottomAnchor, leading: view.layoutMarginsGuide.leadingAnchor, bottom: nil, trailing: nil, padding: .init(top: 20, left: 0, bottom: 0, right: 0), size: .init(width: 150, height: 40))
-        moveGuestListButton.layer.cornerRadius = 5
+        buttonCustomise(button: moveGuestListButton, text: "参加者一覧")
+        moveGuestListButton.anchor(top: moveGuestCardButton.bottomAnchor, leading: view.layoutMarginsGuide.leadingAnchor, bottom: nil, trailing: nil, padding: .init(top: 20, left: 0, bottom: 0, right: 0), size: .init(width: 150, height: 50))
         moveGuestListButton.addTarget(self, action: #selector(showGuestList), for: .touchUpInside)
     }
     
     fileprivate func setupMoveSettingButton() {
         view.addSubview(moveSettingButton)
-        moveSettingButton.setTitle("設定", for: .normal)
-        moveSettingButton.backgroundColor = green
-        moveSettingButton.anchor(top: moveGuestListButton.bottomAnchor, leading: view.layoutMarginsGuide.leadingAnchor, bottom: nil, trailing: nil, padding: .init(top: 20, left: 0, bottom: 0, right: 0), size: .init(width: 150, height: 40))
-        moveSettingButton.layer.cornerRadius = 5
+        buttonCustomise(button: moveSettingButton, text: "設定")
+        moveSettingButton.anchor(top: moveGuestListButton.bottomAnchor, leading: view.layoutMarginsGuide.leadingAnchor, bottom: nil, trailing: nil, padding: .init(top: 20, left: 0, bottom: 0, right: 0), size: .init(width: 150, height: 50))
         moveSettingButton.addTarget(self, action: #selector(showSetting), for: .touchUpInside)
+    }
+    
+    fileprivate func buttonCustomise(button: UIButton, text: String) {
+        button.setTitle(text, for: .normal)
+        button.backgroundColor = green
+        button.titleLabel?.font = .systemFont(ofSize: 24)
+//        button.titleLabel?.adjustsFontSizeToFitWidth = true
+        button.layer.cornerRadius = 5
     }
     
     @objc private func showGuestCard() {
@@ -99,10 +115,6 @@ class EventMenuViewController: UIViewController {
     fileprivate func setBackButtonTitle() {
         let backBarButtonItem = UIBarButtonItem()
         backBarButtonItem.title = "メニューに戻る"
-        // パスワードか何かを要求する？？
-        
-        
-        
         self.navigationItem.backBarButtonItem = backBarButtonItem
     }
     
